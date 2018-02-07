846f3fa71e7177a5dade080a8ed753b8fe6361f7
Deeon Burgan <dburgan@live.com>
2018-02-07 12:57:18 +1100

    Random clear colour button

--- PhysicsMaybe/Imgui.cpp
+++ PhysicsMaybe/Imgui.cpp
@@ -6,6 +6,7 @@
 GUI::GUI()
 {
 	m_clearColour = glm::vec4(1, 1, 1, 1);
+	srand((unsigned)time(0));
 }
 
 
@@ -16,9 +17,22 @@ GUI::~GUI()
 void GUI::update(float* exhaustTimer)
 {
 	ImGui::Begin("My options");
-	glClearColor(m_clearColour.r, m_clearColour.g, m_clearColour.b, 1);
-	ImGui::ColorEdit3("clear colour", glm::value_ptr(m_clearColour));
+
+	if (ImGui::Button("Randomize Colour?"))
+	{
+		float r = rand() % 2 + 0.1f;
+		float g = rand() % 2 + 0.1f;
+		float b = rand() % 2 + 0.1f;
+
+		m_clearColour = glm::vec4(r, g, b, 1);
+	}
+	else
+	{
+		glClearColor(m_clearColour.r, m_clearColour.g, m_clearColour.b, 1);
+		ImGui::ColorEdit3("clear colour", glm::value_ptr(m_clearColour));
+	}
 	ImGui::SliderFloat("Physics Tick Rate", exhaustTimer, 0.01f, 0.1f);
+
 	ImGui::End();
 }
 
